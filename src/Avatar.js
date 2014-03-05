671a277364d1692d254f910df21fdca4ef39cf1b
earthng'eiei <saritsuriya@hotmail.com>
2014-03-05 16:58:05 +0700

    kirby images

---
+++ src/Avatar.js
@@ -0,0 +1,6 @@
+var Avatar = cc.Sprite.extend({
+	ctor: function() {
+		this._super();
+		this.initWithFile( 'images/nest.png' );
+	}
+});
--- src/GameLayer.js
+++ src/GameLayer.js
@@ -2,7 +2,9 @@ var GameLayer = cc.LayerColor.extend({
     init: function() {
         this._super( new cc.Color4B( 127, 127, 127, 255 ) );
         this.setPosition( new cc.Point( 0, 0 ) );
-
+        this.avatar = new Avatar();
+        this.avatar.setPosition( cc.p( screenWidth / 2, screenHeight / 2 ) );
+        this.addChild( this.avatar );
         return true;
     }
 });
