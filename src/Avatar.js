5e6a2e6fc615c751d4e5f9ee209e89ca1811d85f
earthng'eiei <saritsuriya@hotmail.com>
2014-03-05 17:37:58 +0700

    sleeping kirby

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,6 +1,31 @@
-var Avatar = cc.Sprite.extend({
+var Avatar = cc.Node.extend({
 	ctor: function() {
 		this._super();
-		this.initWithFile( 'images/nest.png' );
+		this.nest = cc.Sprite.create( 'images/nest.png' );
+		this.nest.setPosition( new cc.Point( 0, 0 ) );
+		this.addChild( this.nest );
+
+		this.kirby = cc.Sprite.create( 'images/kirby/sleep_kirby01.png');
+		this.kirby.setPosition( new cc.Point( 0, 0 ) );
+		this.addChild( this.kirby );
+
+		this.kirby.movingAction = this.createAnimation();
+		this.kirby.runAction( this.kirby.movingAction );
+	},
+
+	createAnimation: function() {
+		var animation = new cc.Animation.create();
+			animation.addSpriteFrameWithFile( 'images/kirby/sleep_kirby05.png' );
+			animation.addSpriteFrameWithFile( 'images/kirby/sleep_kirby06.png' );
+			animation.addSpriteFrameWithFile( 'images/kirby/sleep_kirby07.png' );
+			animation.addSpriteFrameWithFile( 'images/kirby/sleep_kirby08.png' );
+			animation.addSpriteFrameWithFile( 'images/kirby/sleep_kirby09.png' );
+			animation.addSpriteFrameWithFile( 'images/kirby/sleep_kirby10.png' );
+			animation.addSpriteFrameWithFile( 'images/kirby/sleep_kirby11.png' );
+			console.log('ssss');
+			animation.setDelayPerUnit( 0.4 );
+			return cc.RepeatForever.create( cc.Animate.create( animation ) );
 	}
 });
+
+
